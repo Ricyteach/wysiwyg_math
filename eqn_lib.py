61266633519489be9642a0ca54a511bf3c6e1f32
Rick Teachey <ricky@teachey.org>
2024-02-09 09:42:27 -0500

    Update eqn_lib.py

--- eqn_lib.py
+++ eqn_lib.py
@@ -1,6 +1,6 @@
 from handcalcs.decorator import handcalc
 
-@handcalc(jupyter_display = True, precision = 1)
+@handcalc(jupyter_display = True)
 def NBCC2015LC(DL: float = 0, SDL: float = 0, SL: float = 0, LL: float = 0, WL: float= 0, EL: float = 0):
     LC1 = 1.4*DL
     LC2a = 1.25*DL + 1.5*LL
